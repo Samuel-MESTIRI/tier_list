618913062ec1f01b7d11bebe5b479603f5dc07c0
Samuel-MESTIRI <samuelmestiri@hotmail.fr>
2020-01-28 23:43:30 +0100

    start rank component

---
+++ components/Rank.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="rank border">
+    <div class="rank-title border border-info d-flex align-items-center bg-info text-white h-100">
+      <textarea
+        type="text"
+        class="text-center w-100"
+        maxlength="55"
+        v-model="rankTitle"
+      ></textarea>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        rankTitle: 'Le super titre'
+      }
+    }
+  }
+</script>
+
+<style>
+  .rank {
+    height: 152px;
+    box-sizing: border-box;
+    box-shadow: 0 0 10px rgba(0,0,0,0.25);
+  }
+  .rank-title {
+    width: 20%;
+    resize: none;
+  }
+  .rank-title textarea {
+    resize: none;
+    border: none;
+    background-color: transparent;
+  }
+  .rank-title textarea:focus {
+    outline-color: transparent;
+  }
+</style>
+
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
     <Navbar></Navbar>
+    <div class="container">
+      <div class="col-12 mt-5">
+        <Rank></Rank>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Navbar from '~/components/Navbar.vue'
+  import Navbar from '~/components/Navbar.vue'
+  import Rank from '~/components/Rank.vue'
 
 export default {
   components: {
-    Navbar
+    Navbar,
+    Rank
   }
 }
 </script>
