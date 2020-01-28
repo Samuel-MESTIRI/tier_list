c6cd739604fefb90f35587488dd757dbfb79d0b2
Samuel-MESTIRI <samuelmestiri@hotmail.fr>
2020-01-28 22:38:19 +0100

    init project

---
+++ components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="primary">
+      <b-navbar-brand href="#">Mon super nom de site</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Mon premier line</b-nav-item>
+        </b-navbar-nav>
+
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+
